04b385b004a9eaa63efdce8aebce71be4da4f37c
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-19 14:40:05 +0530

    states

--- src/components/PropsClass.js
+++ src/components/PropsClass.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 export default class PropsClass extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Welcome"
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      message: "Thank You"
+    });
+  }
+
   render() {
     return (
       <div>
-        {this.props.children} {this.props.name}
+        {this.props.children} {this.props.name} {this.state.message}
+        <br />
+        <button onClick={() => this.handleClick()}>Click</button>
       </div>
     );
   }
