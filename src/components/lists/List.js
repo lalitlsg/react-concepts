1b9931c8f4c8099daf9329656d1e359108f3f044
Lalit Garghate <lalit.garghate@gmail.com>
2020-12-02 21:02:20 +0530

    list with index as key

--- src/components/lists/List.js
+++ src/components/lists/List.js
@@ -7,10 +7,23 @@ const List = () => {
     { id: 2, name: "INFO" },
     { id: 3, name: "HCL" }
   ];
+  const colors = ["red", "green", "blue"];
   let companiesList = companies.map(company => (
     <Company key={company.id} company={company} />
   ));
-  return <div>{companiesList}</div>;
+  //list with index as key
+  //only use when list is static, list should not update or sort or filter.
+  let colorList = colors.map((color, index) => (
+    <span key={index} style={{ color: color }}>
+      {color}
+    </span>
+  ));
+  return (
+    <>
+      <div>{companiesList}</div>
+      <div>{colorList}</div>
+    </>
+  );
 };
 
 export default List;
